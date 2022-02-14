88c5ca2ca459e07ca903144252f92845ed27a3df
^_^ <29101894+Kuibagit@users.noreply.github.com>
2022-02-14 18:20:51 +0800

    处理重复修改.vmoptions问题

--- JetBrainsCrack.bat
+++ JetBrainsCrack.bat
@@ -8,6 +8,7 @@ ping -n 5 127.0.0.1>nul
 
 :: JetBrains install root path default
 set jbPath=%ProgramFiles%\JetBrains
+set keysrt=jetbrains-agent.jar
 
 if exist "%jbPath%" (echo [*] JetBrains The installation path is correct: [%jbPath%]) else (echo [-] JetBrains path error! &pause&exit)
 
@@ -17,26 +18,25 @@ set denyDomain=127.0.0.1 account.jetbrains.com jrebel.npegeek.com
 
 echo %denyDomain% >> %hostsfile%
 echo.
-echo [+] Deny JetBrains domain: [account.jetbrains.com、jrebel.npegeek.com]
+echo [*] Deny JetBrains domain: [account.jetbrains.com、jrebel.npegeek.com]
 ping -n 5 127.0.0.1>nul
 echo [+] DenyDoman jetbrains successful.
 echo.
 
 :: Install agent. The default installation path is parent directory: C:\Program Files\JetBrains
 echo f|xcopy %~dp0angent\jetbrains-agent.jar "%jbPath%" /y >nul
-echo [+] Install agent......
+echo [*] Install agent......
 ping -n 5 127.0.0.1>nul
 echo [+] Install agent successful.
 echo.
 
 :: change configuration file and load agent.
-for /R "%jbPath%" %%f in (*.exe.vmoptions) do ( 
+for /r "%jbPath%" %%f in (*.exe.vmoptions) do ( 
     ::echo %%~dpnxa
     echo [*] Finding config: [%%f]
-    xcopy /y "%%f" .\BackupConfig >nul
-    echo -javaagent:%jbPath%\jetbrains-agent.jar>>"%%f"
+    findstr /r /i /c:"%keysrt%" "!%%f!">nul&&echo [-] JetBrains agent already exists.||xcopy /y "!%%f!" .\BackupConfig >nul&&echo -javaagent:%jbPath%\jetbrains-agent.jar>>"!%%f!"&&echo [+] Add successful.
 )
-echo [+] Change config......
+echo [*] Change config......
 ping -n 5 127.0.0.1>nul
 echo [+] Change config loaded agent successful.
 
@@ -44,4 +44,3 @@ echo.
 echo [*] You now can open the JetBrains software and copy the [key.txt] enter to activate it.
 
 pause
-
